9f90ca685c8805ee31c7511aae69fc16a9e28236
Lewis Williams <lewis.williams@totalexpert.com>
2019-11-14 12:41:07 -0600

    random prange alert

--- index.php
+++ index.php
@@ -1,16 +1,36 @@
-<style>
-    .prange {
-        background: url(https://d2leq0htz9909q.cloudfront.net/2018/10/05/16/10/46/782bb582-c31a-4139-a0e5-49a4d4c4693c/RackMultipart20181005-1-sj2pir.png?Expires=1573758806&Signature=QnZRwKd3sx-Vz1MnMaMGvUeTnIuIOUGInFJ6blenOUj~8uOnKoFC--5qlxBs8nqO6I~O~xHtwM5WF3iG4J3G45rVEkAN-43DbAE-vRw0nHjmKmPc8i0uR29LBrfQdpCKuD~lHT1LfIq6o29tKljpuklJ6pXKEHTlc35FZJKCX3EZngRgK45aXTCGjlMQM51dpUyho-~2UCxM2rPwJAxO5ACTZfUTE69l~XVQ72DzlkBgVdqh7do376RE9VwV9CUynG3DbJwQSz67Wf1ZyxVXoTe1jTHrDm0NIYZEo0y9it41TCKTqNnOKpGoqc7wxeAyxhB916Q7Gb50whYsbssdIQ__&Key-Pair-Id=APKAILIVQHXAC6ED4RWQ);
-        width: 400;
-        height: 400;
-    }
-</style>
 <?php 
 include 'haircuts.php';
+include 'prange.php';
+$prange = get_prange();
 $haircut = get_haircut();
-echo "<img class=prange src=\"$haircut\" width=400><br>";
+
+echo "
+<style>
+    .prange {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 400;
+        height: 400;
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -ms-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+
+    img.resize {
+        width:400;
+        height:400px;
+      }
+</style>
+";
+
+echo "
+    <img class=prange src=\"$prange\">
+    <img class=prange src=\"$haircut\">";
 
 ?>
-
 
 refresh for another sick do
